8c5fe99a004bb387e74d5e1593350b98e76b7334
Gael Bosser <bosser.gael@gmail.com>
2020-03-19 14:20:24 +0100

    Action swipe sur un gameobject

---
+++ Assets/Script/SwipeScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SwipeScript : MonoBehaviour
+{
+	private readonly float rotationSpeed = 10f;
+	private Quaternion originalRotationValueCube;
+
+    void Start()
+    {
+		originalRotationValueCube = transform.rotation;
+	}
+
+    void OnMouseDrag()
+	{
+		float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+		transform.Rotate(Vector3.right, YaxisRotation);
+	}
+
+    public void ResetPositionGameObject(GameObject gameObject)
+    {
+		gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, originalRotationValueCube, Time.time * 1.0f);
+	}
+}
+
--- Assets/Script/TouchScript.cs
+++ Assets/Script/TouchScript.cs
@@ -9,6 +9,7 @@ public class TouchScript : MonoBehaviour
     private Vector3 middlePosition;
     private Vector3 bigScale;
     private Vector3 minScale;
+    private bool elementIsFocused;
 
     private void Start()
     {
@@ -27,9 +28,8 @@ public class TouchScript : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit, 100))
+            if (Physics.Raycast(ray, out RaycastHit hit, 100))
             {
                 // whatever tag you are looking for on your game object
                 if (hit.collider.gameObject.name == "Cube")
@@ -42,45 +42,68 @@ public class TouchScript : MonoBehaviour
                     cube.transform.localPosition = middlePosition;
 
                     cube.GetComponent<RotateScript>().CanRotate = false;
+
+                    if (!elementIsFocused)
+                    {
+                        elementIsFocused = true;
+                        cube.GetComponent<SwipeScript>().ResetPositionGameObject(cube);
+                    }
                 }
                 else if (hit.collider.gameObject.name == "Sphere")
                 {
                     Debug.Log("Sphere");
+
                     cube.SetActive(false);
                     cylindre.SetActive(false);
+
                     sphere.transform.localScale = new Vector3(1, 0.26f, 1);
                     sphere.GetComponent<RotateScript>().CanRotate = false;
+
+                    if (!elementIsFocused)
+                    {
+                        elementIsFocused = true;
+                        sphere.GetComponent<SwipeScript>().ResetPositionGameObject(sphere);
+                    }
                 }
                 else if (hit.collider.gameObject.name == "Cylinder")
                 {
                     Debug.Log("Cylinder");
+
                     sphere.SetActive(false);
                     cube.SetActive(false);
+
                     cylindre.transform.localScale = bigScale;
                     cylindre.transform.localPosition = middlePosition;
+
                     cylindre.GetComponent<RotateScript>().CanRotate = false;
+                    if (!elementIsFocused)
+                    {
+                        elementIsFocused = true;
+                        cylindre.GetComponent<SwipeScript>().ResetPositionGameObject(cylindre);
+                    }
                 }
                 else
                 {
                     sphere.SetActive(true);
                     sphere.transform.localScale = new Vector3(0.25f, 0.07f, 0.25f);
                     sphere.GetComponent<RotateScript>().CanRotate = true;
+                    sphere.GetComponent<SwipeScript>().ResetPositionGameObject(sphere);
 
                     cube.SetActive(true);
                     cube.transform.localScale = minScale;
                     cube.transform.localPosition = new Vector3(496.83f, 1.97f, 41.11f);
                     cube.GetComponent<RotateScript>().CanRotate = true;
+                    cube.GetComponent<SwipeScript>().ResetPositionGameObject(cube);
 
                     cylindre.SetActive(true);
                     cylindre.transform.localScale = minScale;
                     cylindre.transform.localPosition = new Vector3(498.93f, 1.97f, 41.11f);
                     cylindre.GetComponent<RotateScript>().CanRotate = true;
+                    cylindre.GetComponent<SwipeScript>().ResetPositionGameObject(cylindre);
 
+                    elementIsFocused = false;
                 }
             }
         }
     }
-
-
-
 }
