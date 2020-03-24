e271ba822f8e0035f11c8a6944a90bca0129ece3
Gael Bosser <bosser.gael@gmail.com>
2020-03-24 10:51:47 +0100

    Ajout MaterialScript

---
+++ Assets/Script/MaterialScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class MaterialScript : MonoBehaviour
+{
+    [SerializeField]
+    private Texture[] textures;
+    private int indexSelected = 0;
+
+    void OnMouseDown()
+    {
+        if (textures.Any())
+        {
+            if(indexSelected == textures.Length - 1)
+            {
+                indexSelected = 0;
+            }
+            else
+            {
+                indexSelected++;
+            }
+
+            GetComponent<Renderer>().material.mainTexture = textures[indexSelected];
+        }
+    }
+}
